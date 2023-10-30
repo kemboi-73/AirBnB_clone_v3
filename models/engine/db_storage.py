3afbda5455a987370da48dee164bfad18bccfdee
Mikey_254 <123319720+kemboi-73@users.noreply.github.com>
2023-10-30 07:44:15 +0300

    update_of_storage_engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,24 +71,22 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
-    def get(self, cls, id):
-        """retrieve one object"""
-        if cls in classes:
-            obj = self.__session.query(classes[cls]).filter_by(id=id).first()
-            if obj is not None:
-                return obj
-        return None
-
-    def count(self, cls=None):
-        """count the number of objects in storage"""
-        if cls is not None and cls in classes:
-            count = self.__session.query(classes[cls]).count()
-        else:
-            count = 0
-            for cls in classes.values():
-                count += self.__session.query(cls).count()
-        return count
-
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ retrieves """
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """ counts """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
